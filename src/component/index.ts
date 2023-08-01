ce3fe1c00935c1fa11aef71c3ba6e5a261c0d970
Fernando Muñoz <munioz.fernando@gmail.com>
2023-08-01 11:27:16 +0200

    fix: update

--- src/component/index.ts
+++ src/component/index.ts
@@ -1,3 +1,1 @@
 export { default as Button } from './button';
-export { default as meta } from './button.stories';
-export { Default } from './button.stories';
---
+++ src/storybook/button.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Button from '../component/button';
+
+type Story = StoryObj<typeof Button>;
+
+export default {
+  title: 'Components/Button',
+  component: Button
+} as Meta<typeof Button>;
+
+export const DefaultButton: Story = {
+  args: {
+    text: 'Button text !'
+  }
+};
